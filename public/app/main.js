c7abb8194aa6f19fe87a116f8b4fbcee395e635f
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-01 12:03:11 -0500

    route parameters

--- public/app/main.js
+++ public/app/main.js
@@ -11,9 +11,14 @@ angular.module("app", ["ngRoute"])   //eslint-disable-line no-unused-vars
         template: "<a href='#/'>Home</a><h1>{{header}}</h1>",
         controller: "MainCtrl"
       })
+      .when("/hello/:name", {
+        template: "<a href='#/'>Home</a><h1>{{header}}</h1>",
+        controller: "MainCtrl"
+      })
       .otherwise("/");
   })
 
-  .controller("MainCtrl", function($scope) {
-    $scope.header = "Hello Angular World";
+  .controller("MainCtrl", function($scope, $routeParams) {
+    const { name } = $routeParams;  //NOTE(adam): destructuring because why not
+    $scope.header = `Hello ${name || "Angular World"}`;
   });
