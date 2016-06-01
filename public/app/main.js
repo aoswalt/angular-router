54ea986084c8bbd9aec1f3123fbe6b41a6ad5810
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-01 11:48:14 -0500

    basic routing

--- public/app/main.js
+++ public/app/main.js
@@ -2,6 +2,18 @@
 
 //NOTE(adam): array is dependancies
 angular.module("app", ["ngRoute"])   //eslint-disable-line no-unused-vars
+  .config($routeProvider => {
+    $routeProvider
+      .when("/", {
+        template: "<h1>{{header}}</h1>",
+        controller: "MainCtrl"
+      })
+      .when("/hello", {
+        template: "<h1>OMG No Controller</h1>"
+      })
+      .otherwise({});
+  })
+
   .controller("MainCtrl", function($scope) {
     $scope.header = "Hello World";
   });
