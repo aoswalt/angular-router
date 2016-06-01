7612663e6ff738f36acacc973907367685760cc3
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-01 13:37:20 -0500

    template urls instead of hardcoded strings

--- public/app/main.js
+++ public/app/main.js
@@ -1,29 +1,24 @@
 /* global angular:false */
 
-//NOTE(adam): array is dependancies
 angular.module("app", ["ngRoute"])   //eslint-disable-line no-unused-vars
   .config($routeProvider => {
+    //NOTE(adam): partials are not loaded until requested, can be configured differently
     $routeProvider
       .when("/", {
-        template: "<a href='#/hello'>Hello</a><h1>{{'Home Sweet Home' | uppercase}}</h1>"
+        templateUrl: "/app/partials/home.html"
       })
       .when("/hello", {
-        template: `Say hello to:
-          <form ng-submit="hello()">
-            <input ng-model="name">
-            <input type="submit"
-          </form>`,
+        templateUrl: "/app/partials/hello.html",
         controller: "HelloCtrl"
       })
       .when("/hello/:name", {
-        template: "<a href='#/'>Home</a> <a href='#/hello/'>Back</a><h1>{{header}}</h1>",
+        templateUrl: "/app/partials/hello-person.html",
         controller: "HelloPersonCtrl"
       })
       .otherwise("/");
   })
 
   .controller("HelloCtrl", function($scope, $location) {
-    // $scope.hello = () => window.location.assign(`#/hello/${$scope.name}`);
     $scope.hello = () => $location.path(`/hello/${$scope.name}`);
   })
 
